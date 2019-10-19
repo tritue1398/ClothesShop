f7aa191c66d3076aac77289959eb1f6ba09b6158
tritue1398 <37269906+tritue1398@users.noreply.github.com>
2019-10-19 17:09:59 +0700

    Add project files.

---
+++ Clothes_Shop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Clothes_Shop.MvcApplication" Language="C#" %>
---
+++ Clothes_Shop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Clothes_Shop.Startup))]
+namespace Clothes_Shop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
