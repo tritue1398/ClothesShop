e78897885f510fd661237824b25bf9cd85346f25
tritue1398 <37269906+tritue1398@users.noreply.github.com>
2019-11-06 16:53:02 +0700

    Create=interface

---
+++ Clothes_Shop/Controllers/FooterController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clothes_Shop.Controllers
+{
+    public class FooterController : Controller
+    {
+        // GET: Footer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PayMothod()
+        {
+            return View();
+        }
+
+        public ActionResult TransitMothod()
+        {
+            return View();
+        }
+        public ActionResult Address()
+        {
+            return View();
+        }
+    }
+}
---
+++ Clothes_Shop/Controllers/TrangChuController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Clothes_Shop.Models;
+
+namespace Clothes_Shop.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        ClotherShopEntities db = new ClotherShopEntities();
+        // GET: TrangChu
+        public ActionResult Index()
+        {
+            var sp = (from p in db.SANPHAMs orderby p.MASP descending select p).Take(2).ToList();
+            return View();
+        }
+
+        public ActionResult ChiTietSanPham()
+        {
+            return View();
+        }
+    }
+}
